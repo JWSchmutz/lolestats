b91728690e6110c7ee0ac3779acd2ec36c5bc5ea
jwschmutz <jeremiah.schmutz@gmail.com>
2022-11-12 23:49:32 -0500

    trying hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Teams from "./pages/Teams";
 import Players from "./pages/Players";
@@ -38,6 +38,33 @@ function App() {
         </main>
       </div>
     </Router>
+
+    // <Router>
+    //   <div className="app">
+    //     <Navbar
+    //       title="LoL EStats"
+    //       pages={["home", "players", "teams", "tournaments"]}
+    //     />
+
+    //     <main className="app-main">
+    //       <Routes>
+    //         <Route path="/" element={<Home />}></Route>
+    //         <Route path="/lolestats" element={<Home />}></Route>
+    //         <Route path="/teams" element={<Teams />}></Route>
+    //         <Route path="/players" element={<Players />}></Route>
+    //         <Route path="/tournaments" element={<Tournaments />}></Route>
+    //         <Route
+    //           path="/tournaments/worlds/:eventName"
+    //           element={<WorldsEvent />}
+    //         ></Route>
+    //         <Route
+    //           path="/tournaments/msi/:eventName"
+    //           element={<MsiEvent />}
+    //         ></Route>
+    //       </Routes>
+    //     </main>
+    //   </div>
+    // </Router>
   );
 }
 
